3abdd3f3bf7f0a3424490dd22d737bde775803fa
adamgoat <goad.adam@gmail.com>
2018-12-09 01:51:01 -0600

    Add drive code for claw and lift

--- drive/rpiPWM1/closeClaw.py
+++ drive/rpiPWM1/closeClaw.py
@@ -9,11 +9,5 @@ def SetAngle(angle):
     duty = angle / 18 + 2
     GPIO.output(18,True)
     pwm.ChangeDutyCycle(duty)
-    sleep(2)
-    GPIO.output(18,False)
-    pwm.ChangeDutyCycle(0)
-SetAngle(80)
+    sleep(5)
 SetAngle(30)
-SetAngle(60)
-pwm.stop()
-GPIO.cleanup()
